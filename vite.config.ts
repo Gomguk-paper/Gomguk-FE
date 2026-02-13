aedfd96934ea3d8577a17a64a7ce704731a3d199
Juwon Kwon <juwonkwon.dev@gmail.com>
2026-02-13 17:55:44 +0900

    :bug: FIX: Update API proxy target port to 8081

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://127.0.0.1:8081',
         changeOrigin: true,
         //rewrite: (path) => path.replace(/^\/api/, ''),
       },
