0bd06e156564b41cb2274758c76178594e844b0e
Juwon Kwon <juwonkwon.dev@gmail.com>
2026-02-13 18:28:31 +0900

    :ambulance: 잘못 수정한 포트 변경

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ import path from "path";
 export default defineConfig({
   server: {
     host: "::",
-    port: 8080,
+    port: 8081,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8081',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         //rewrite: (path) => path.replace(/^\/api/, ''),
       },
